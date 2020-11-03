86d9e686a9e695c089cb984721256dc024e431b0
Steven Fernández <victor.fernandezespinoza@ucr.ac.cr>
2020-11-03 00:06:16 -0600

    Código más compacto

--- proyecto-1/source/GameController.c
+++ proyecto-1/source/GameController.c
@@ -1,6 +1,6 @@
 #include <tonc.h>
-#include "GameController.h"
 
+#include "GameController.h"
 #include "Sprite.h"
 #include "Rect.h"
 #include "Menu.h"
@@ -83,16 +83,16 @@ int gamectrl_run()
 
 void gamectrl_start()
 {
-	// char to put in screen
+	// Text buffer
 	char totalScore[100]; 
 
-	int start = 0;
-	int ii= 0;
+	bool start = false;
+	bool win = false;
+
 	int hScroll = 0;
 	int h2Scroll = 0;
-	int win=0;
-    OBJ_ATTR * oe = (OBJ_ATTR *)oam_mem;
-	
+    u32 frame_counter = 0;
+
 	while(1)
 	{
 		VBlankIntrWait();
@@ -101,97 +101,117 @@ void gamectrl_start()
 
 		if(!start)
 		{
-			if(win){
-				REG_BG1_SCROLL_H = hScroll;
-				hScroll +=1;
-			}
-			for(ii=0; ii<HWLEN; ii++){
-				pat_bounce(&pats[ii]);
-				oe[ii].attr0 &= ~ATTR0_Y_MASK;
-				oe[ii].attr0 |= (pats[ii].fy>>8)& ATTR0_Y_MASK;
-			}
+			if(win)
+				REG_BG1_SCROLL_H = hScroll = hScroll + 1;
 
-			// Skip start screen and print instructions
-			if(key_hit(KEY_START)){	
-				print_instructions(oe);
-			}
-			// Start Game
-			if(key_hit(KEY_A)){
-				oam_copy(oe, 0, 12);
-				sprite_coin_init(&coin, &obj_buffer[1]);
-				start=1;
-			}
+            if((start = gamectrl_show_main_menu()))
+            {
+                // Clean text
+			    tte_write("#{es}");
+
+                // DMA-copy the background
+                dma3_cpy(pal_bg_mem, twoCloudPal, twoCloudPalLen);
+                dma3_cpy(tile_mem[0], twoCloudTiles, twoCloudTilesLen);
+                dma3_cpy(se_mem[30], twoCloudMap, twoCloudMapLen);
+            }
 		}
 
-		if(start){
-			tte_write("#{es}");
+		if(start)
+        {
+			gamectrl_show_first_lvl(totalScore, &frame_counter, &h2Scroll);
 
-			for(int i = 0; i < BLOCKS_AMOUNT; ++i)
-				rect_paint(&bgen.blocks[i]);
+            if(coin.currentScore == 3)
+            {
+                win = true;
 
-			// Load game background
-			dma3_cpy(pal_bg_mem, twoCloudPal, twoCloudPalLen);
-			dma3_cpy(tile_mem[0], twoCloudTiles, twoCloudTilesLen);
-			dma3_cpy(se_mem[30], twoCloudMap, twoCloudMapLen);
-	
-			int do_scroll = blockgen_autoscroll(&bgen);
-			if(do_scroll) sprite.pos_y += 1;
+                REG_BG1_SCROLL_V = 0;
+                REG_BG1_SCROLL_H = 0;
+                hScroll = 0;
+                h2Scroll = 0;
 
-			Rect * rects = bgen.blocks;
-			sprite_update_pos_collision(&sprite, &rects, BLOCKS_AMOUNT);
-			sprite_change_animation(&sprite);
+                // Sets the end game screen to be shown
+                dma3_cpy(pal_bg_mem, winPal, winPalLen);
+                dma3_cpy(tile_mem[0], winTiles, winTilesLen);
+                dma3_cpy(se_mem[30], winMap, winMapLen);
 
-			// Change coin animation
-			sprite_coin_update_pos(&coin);
-			sprite_coin_change_animation(&coin);
-			// Detect coin-sprite collision
-			if(do_sprites_collisions(&coin,&sprite)){
-				// Write in screen, position x = 0, y = 0
-				snprintf(totalScore, 100, "#{P:0, 0}Coins:%d", coin.currentScore);
-				tte_write(totalScore);
-			}
-			sprite_coin_unhide(&coin, &sprite);
+            }
+            else if(sprite.pos_y > 160)
+            {
+                win = false;
+                // Change palette
+                dma3_cpy(pal_bg_mem, twoCloudgrayPal, twoCloudgrayPalLen);
+            }
 
-			// Move the sprites to VRAM. Player + coin + blocks
-			oam_copy(oam_mem, obj_buffer, SPRITES_AMOUNT);
-			
-			// Move background vertical
-		    REG_BG1_SCROLL_V = h2Scroll;
-			hScroll -=1;
-			h2Scroll = hScroll/5;
+            if(coin.currentScore == 3 || sprite.pos_y > 160)
+            {
+                final_screen(oam_mem, coin.currentScore, SPRITES_AMOUNT);
+                sprite_place_on_rect(&sprite, blockgen_get_topmost_block8(&bgen, 0));
+                sprite_coin_init_with_colis(&coin, &obj_buffer[1],&sprite);
 
-		}
-
-		if(coin.currentScore==3)
-		{
-			// Sets the end game screen to be shown in this case game won
-			final_screen(oam_mem, coin.currentScore, SPRITES_AMOUNT);
-			sprite_place_on_rect(&sprite, blockgen_get_topmost_block8(&bgen, 0));
-			sprite_coin_init_with_colis(&coin, &obj_buffer[1],&sprite);
-			sprite.jumps = 0;
-			start=0;
-
-			REG_BG1_SCROLL_V = 0;
-			REG_BG1_SCROLL_H = 0;
-			hScroll = 0;
-			h2Scroll = 0;
-			win=1;
-			dma3_cpy(pal_bg_mem, winPal, winPalLen);
-			dma3_cpy(tile_mem[0], winTiles, winTilesLen);
-			dma3_cpy(se_mem[30], winMap, winMapLen);
-
-		}
-		else if(sprite.pos_y > 160)
-		{
-			// Sets the end game screen to be shown in this case game lost
-			final_screen(oam_mem, coin.currentScore, SPRITES_AMOUNT);
-			sprite_place_on_rect(&sprite, blockgen_get_topmost_block8(&bgen, 0));
-			sprite_coin_init_with_colis(&coin, &obj_buffer[1],&sprite);
-			sprite.jumps = 0;
-			start=0;
-			win = 0;
-			// Change palette
-			dma3_cpy(pal_bg_mem, twoCloudgrayPal, twoCloudgrayPalLen);
-		}
+                sprite.jumps = 0;
+                start = false;
+            }
+        }
 	}
+}
+
+bool gamectrl_show_main_menu()
+{
+    OBJ_ATTR * oe = (OBJ_ATTR *)oam_mem;
+
+    // Tiquicia Jump bounce text
+    for(int ii = 0; ii < HWLEN; ii++)
+    {
+        pat_bounce(&pats[ii]);
+        oe[ii].attr0 &= ~ATTR0_Y_MASK;
+        oe[ii].attr0 |= (pats[ii].fy>>8)& ATTR0_Y_MASK;
+    }
+
+    // Skip start screen and print instructions
+    if(key_hit(KEY_START)){	
+        print_instructions(oe);
+    }
+
+    // Start Game
+    if(key_hit(KEY_A)){
+        oam_copy(oe, 0, 12);
+        sprite_coin_init(&coin, &obj_buffer[1]);
+        return true;
+    }
+
+    return false;
+}
+
+void gamectrl_show_first_lvl(char * totalScore, u32 * frame_counter, int * h2Scroll)
+{
+    for(int i = 0; i < BLOCKS_AMOUNT; ++i)
+        rect_paint(&bgen.blocks[i]);
+
+    // If the blocks scrolled, scroll the player as well
+    if(blockgen_autoscroll(&bgen)) sprite.pos_y += 1;
+
+    Rect * rects = bgen.blocks;
+    sprite_update_pos_collision(&sprite, &rects, BLOCKS_AMOUNT);
+    sprite_change_animation(&sprite);
+
+    // Change coin animation
+    sprite_coin_update_pos(&coin);
+    sprite_coin_change_animation(&coin);
+
+    // Detect coin-sprite collision
+    if(do_sprites_collisions(&coin,&sprite)){
+        // Write in screen, position x = 0, y = 0
+        snprintf(totalScore, 100, "#{P:0, 0}Coins:%d", coin.currentScore);
+        tte_write(totalScore);
+    }
+
+    sprite_coin_unhide(&coin, &sprite);
+
+    // Move the sprites to VRAM. Player + coin + blocks
+    oam_copy(oam_mem, obj_buffer, SPRITES_AMOUNT);
+
+    *frame_counter = (*frame_counter + 1) % 5;
+
+    // Move background vertical
+    REG_BG1_SCROLL_V = *h2Scroll += *frame_counter == 0 ? 1 : 0;
 }
--- proyecto-1/source/GameController.h
+++ proyecto-1/source/GameController.h
@@ -1,16 +1,21 @@
 #ifndef GAME_CONTROLLER_H
 #define GAME_CONTROLLER_H
 
+#include <tonc.h>
+
 #define REG_BG1_SCROLL_H       *((volatile u16*)(0x04000014))
 #define REG_BG1_SCROLL_V       *((volatile u16*)(0x04000016))
 
 // Player + coin + blocks
 #define SPRITES_AMOUNT (1 + 1 + BLOCKS_AMOUNT)
 
+int gamectrl_run();
+void gamectrl_init();
 void gamectrl_init_regs();
 void gamectrl_init_interrupts();
-void gamectrl_init();
 void gamectrl_start();
-int gamectrl_run();
+
+bool gamectrl_show_main_menu();
+void gamectrl_show_first_lvl(char * totalScore, u32 * frame_counter, int * h2Scroll);
 
 #endif
