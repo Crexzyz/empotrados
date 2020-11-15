a227309fbb6525c3d9ff71bcfbadfd601deb1b07
Steven Fernández <victor.fernandezespinoza@ucr.ac.cr>
2020-11-15 00:20:25 -0600

    Mejora final en la interfaz de alarma

--- reloj/source/Runner.c
+++ reloj/source/Runner.c
@@ -25,6 +25,16 @@ void run_init()
 	tte_init_se_default(0, BG_CBB(0) | BG_SBB(31));
 }
 
+void runner_rainbow_print(Clock * clock, char * buffer, NumberPrinter * np, u8 x, u8 y, size_t len)
+{
+	update_clock(clock);
+	
+	snprintf(buffer, len, "%02d:%02d:%02d",
+		clock->hours, clock->minutes, clock->seconds);
+	
+	np_rainbow_print(np, x, y, buffer, strnlen(buffer, len));
+}
+
 void run_clock(DispCtrl * dc)
 {
     char buf[SPRITE_BUFFER_SIZE + 1]; buf[SPRITE_BUFFER_SIZE] = 0;
@@ -56,13 +66,7 @@ void run_clock(DispCtrl * dc)
 		
 		if(frame_counter % 10 == 0)
 		{
-			update_clock(&clock);
-			
-			snprintf(buf, SPRITE_BUFFER_SIZE + 1, "%02d:%02d:%02d",
-				clock.hours, clock.minutes, clock.seconds);
-			
-			np_rainbow_print(&np, 50, 30, buf, strnlen(buf, SPRITE_BUFFER_SIZE + 1));
-
+			runner_rainbow_print(&clock, buf, &np, 50, 30, SPRITE_BUFFER_SIZE + 1);
 			oam_copy(oam_mem, obj_buffer, SPRITE_BUFFER_SIZE);
 		}
 
@@ -131,6 +135,7 @@ void run_stopwatch(DispCtrl * dc)
 
 void run_alarm(DispCtrl * dc)
 {
+	char buf[SPRITE_BUFFER_SIZE + 1]; buf[SPRITE_BUFFER_SIZE] = 0;
 	const u8 options_count = 2;
 
 	TimeChooser tc;
@@ -138,8 +143,11 @@ void run_alarm(DispCtrl * dc)
 	OptionText options[options_count];
 	OptionFunction functions[options_count];
 	OptsChser oc;
-
+	NumberPrinter np_clock;
+	Clock clock;
 	OBJ_ATTR * sprite_buf = &obj_buffer[SPRITE_BUFFER_SIZE + 1];
+	
+    init_clock(&clock);
 
 	OptionText_init(&options[0], "Back", strlen("Back"));
 	OptionText_init(&options[1], "Set alarm", strlen("Set alarm"));
@@ -148,35 +156,48 @@ void run_alarm(DispCtrl * dc)
 	OptFunc_init(&functions[1], TimeChooser_toggle_edit, &tc);
 
 	OptsChser_init(&oc, options, options_count, functions);
-	OptsChser_set_coords(&oc, 0, 100);
+	OptsChser_set_coords(&oc, 0, 110);
 
+	np_init(&np_clock, &obj_buffer[0], SPRITE_BUFFER_SIZE);
 	np_init(&np, sprite_buf, TIME_CHOOSER_BUF_SIZE - 1);
-	TimeChooser_init(&tc, &np, 50, 30);
+
+	TimeChooser_init(&tc, &np, 112, 70);
+
+	tte_write("#{P:0,40}Clock");
+	tte_write("#{P:0,78}Current\nalarm");
+	size_t frame_counter = 0;
 
 	while(true)
 	{
 		VBlankIntrWait();
-
 		key_poll();
+		frame_counter = (frame_counter + 1) % 60;
+
 		TimeChooser_show(&tc);
+
+		if(frame_counter % 10 == 0)
+			runner_rainbow_print(&clock, buf, &np_clock, 112, 40, SPRITE_BUFFER_SIZE + 1);
 
 		if(!tc.editing)
 		{
 			// Prevents listening for keystrokes on the menu
 			OptsChser_show(&oc);
-			tte_write("#{P:0,150}         ");
+			tte_write("#{P:104,126}          ");
 		}
 		else
 		{
-			tte_write("#{P:0,150} A: Save");
+			tte_write("#{P:0,126} A: Save   D-Pad: Move");
 		}
 
+		oam_copy(oam_mem, obj_buffer, SPRITE_BUFFER_SIZE);
 		oam_copy(&oam_mem[SPRITE_BUFFER_SIZE], sprite_buf, TIME_CHOOSER_BUF_SIZE - 1);
 		if(dc->content_change == true)
 			break;
 	}
 
+	oam_copy(oam_mem, 0, SPRITE_BUFFER_SIZE);
 	oam_copy(&oam_mem[SPRITE_BUFFER_SIZE], 0, TIME_CHOOSER_BUF_SIZE);
+
 	dc->content_change = false;
 }
 
